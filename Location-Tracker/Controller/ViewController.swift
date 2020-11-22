5bcd2b4b5cf9d4d640013b4751394826c9118652
Kevin Jonathan <kevinjonathan3010@gmail.com>
2020-11-22 17:10:48 +0700

    Login mechanism success, added alertController

--- Location-Tracker/Controller/ViewController.swift
+++ Location-Tracker/Controller/ViewController.swift
@@ -10,22 +10,53 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
+    
+    var ref: DatabaseReference!
 
     @IBOutlet weak var textUsername: UITextField!
     @IBOutlet weak var textPassword: UITextField!
     @IBAction func btnSignIn(_ sender: UIButton) {
         if(textUsername.text != "" && textPassword.text != "") {
-            print("Hello \(textUsername.text!), your password is \(textPassword.text!)")
+            //print("Hello \(textUsername.text!), your password is \(textPassword.text!)")
+            ref.child(textUsername.text!).observe(.value, with: { (snapshot) in
+                if(snapshot.childrenCount > 0) {
+                    let v = snapshot.value as! NSDictionary
+                    //print(v as Any)
+                    for (_,j) in v {
+                        //print(j)
+                        for (m,n) in j as! NSDictionary {
+                            if(m as! String == "password") {
+                                if(n as! String == self.textPassword.text!) {
+                                    let alert = UIAlertController(title: "Login Success", message: "Anda terlogin sebagai \(self.textUsername.text!)", preferredStyle: .alert)
+                                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                                    self.present(alert, animated: true, completion: nil)
+                                } else {
+                                    let alert = UIAlertController(title: "Error", message: "Password salah!", preferredStyle: .alert)
+                                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                                    self.present(alert, animated: true, completion: nil)
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    let alert = UIAlertController(title: "Error", message: "Username salah!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+              }) { (error) in
+                print(error.localizedDescription)
+            }
         }
         else {
-            print("Please fill the required field!")
+            let alert = UIAlertController(title: "Error", message: "Please fill the required field!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
--- Location-Tracker/Controller/ViewControllerRegister.swift
+++ Location-Tracker/Controller/ViewControllerRegister.swift
@@ -21,12 +21,18 @@ class ViewControllerRegister: UIViewController {
             if(textPassword.text == textReenterPassword.text) {
                 let value = ["username": textUsername.text!,	"password": textPassword.text!]
                 ref.child(textUsername.text!).child("userProfile").setValue(value)
-                print("Register Success, Username: \(textUsername.text!), Password: \(textPassword.text!)")
+                let alert = UIAlertController(title: "Register Success", message: "Registration is done, Username: \(textUsername.text!)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             } else {
-                print("Password doesn't match")
+                let alert = UIAlertController(title: "Error", message: "Password doesn't match", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }
         } else {
-            print("Please fill the required field!")
+            let alert = UIAlertController(title: "Error", message: "Please fill the required field!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     override func viewDidLoad() {
@@ -34,16 +40,4 @@ class ViewControllerRegister: UIViewController {
         ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

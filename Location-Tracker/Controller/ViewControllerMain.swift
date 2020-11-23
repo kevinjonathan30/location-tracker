6afcb9be2e511a1f2fa21ea69d8e4b22465afbf0
Kevin Jonathan <kevinjonathan3010@gmail.com>
2020-11-23 15:43:57 +0700

    Multiple annotation in map is now working, can also delete annotations

--- Location-Tracker/Controller/ViewControllerMain.swift
+++ Location-Tracker/Controller/ViewControllerMain.swift
@@ -19,18 +19,27 @@ class ViewControllerMain: UIViewController, CLLocationManagerDelegate {
     
     var myUsername : String = ""
     var delegate: backDelegate?
+    var ref: DatabaseReference!
     var locationManager: CLLocationManager!
     var latitude = ""
     var longitude = ""
+    let annotationLocations = [
+        ["title": "Pinned", "latitude": -26.2000, "longitude": 28.0450],
+        ["title": "Pinned", "latitude": -26.2100, "longitude": 28.0500]
+    ]
 
     @IBOutlet weak var textUsername: UILabel!
     @IBAction func btnSaveLocation(_ sender: UIButton) {
         locationManager.startUpdatingLocation()
-        let currentDate = NSDate.now
-        print(currentDate)
+        let today = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm E, d MMM y"
+        let value = ["date": formatter.string(from: today), "latitude": latitude, "longitude": longitude] as [String : Any]
+        ref.child(myUsername).child("savedLocations").childByAutoId().setValue(value)
     }
     @IBAction func btnHapusDatabaseTracker(_ sender: UIButton) {
-        self.myMap.removeAnnotations(self.myMap.annotations)
+        let annotations = myMap.annotations.filter({ !($0 is MKUserLocation) })
+        myMap.removeAnnotations(annotations)
     }
     @IBAction func btnSignOut(_ sender: UIButton) {
         delegate?.resetSignInStatus(data:true)
@@ -50,11 +59,16 @@ class ViewControllerMain: UIViewController, CLLocationManagerDelegate {
             let mapSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
             let mapRegion = MKCoordinateRegion(center: mapCoord, span: mapSpan)
             self.myMap.setRegion(mapRegion, animated: true)
-            let anotasi = MKPointAnnotation()
-            anotasi.title = "My Location"
-            anotasi.subtitle = "I'm here"
-            anotasi.coordinate = mapCoord
-            self.myMap.addAnnotation(anotasi)
+            generateAnnotations()
+        }
+    }
+    
+    func generateAnnotations() {
+        for location in annotationLocations {
+            let annotations = MKPointAnnotation()
+            annotations.title = location["title"] as? String
+            annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
+            myMap.addAnnotation(annotations)
         }
     }
 
@@ -66,14 +80,13 @@ class ViewControllerMain: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true)
+        ref = Database.database().reference()
         textUsername.text = "Hello, \(myUsername)!"
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        let currentDate = NSDate.now
-        print(currentDate)
     }
     
 
